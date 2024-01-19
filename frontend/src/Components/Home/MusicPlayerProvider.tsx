dc562ac2d98772a8a2733d1027a2492fc5b96069
Parmeshwar Murmu <murmuparmeshwar05@gmail.com>
2024-01-19 20:12:48 +0530

    Update_Playlist

--- frontend/src/Components/Home/MusicPlayerProvider.tsx
+++ frontend/src/Components/Home/MusicPlayerProvider.tsx
@@ -9,13 +9,15 @@ import { FaVolumeLow } from "react-icons/fa6";
 
 export const MusicPlayerProvider = () => {
 
-    
+
 
     const { currentTrack, showMusicPlayer } = useContext(appContent);
     const audioRef = useRef<HTMLAudioElement>(null);
     const [isPlaying, setIsPlaying] = useState(false);
     const [currentTime, setCurrentTime] = useState(0);
     const [duration, setDuration] = useState(0);
+    const [volume, setVolume] = useState<number>(1);
+    const [isVolumeControlVisible, setIsVolumeControlVisible] = useState<boolean>(false);
 
     useEffect(() => {
         if (currentTrack && audioRef.current) {
@@ -59,11 +61,29 @@ export const MusicPlayerProvider = () => {
         }
     };
 
+
+    const handleVolumeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (audioRef.current) {
+            const newVolume = Number(e.target.value);
+            audioRef.current.volume = newVolume;
+            setVolume(newVolume);
+        }
+    };
+
+
+    const handleVolumeIconMouseEnter = () => {
+        setIsVolumeControlVisible(true);
+    };
+
+    const handleVolumeIconMouseLeave = () => {
+        setIsVolumeControlVisible(false);
+    };
+
     return (
 
         <div className=''>
 
-            
+
 
 
             {currentTrack && (<div className='flex'>
@@ -73,54 +93,82 @@ export const MusicPlayerProvider = () => {
                 </div>
 
                 <div className='w-full'>
-
-                <div className="flex justify-center items-center">
-                    <span>{formatTime(currentTime)}</span>
-                    <input className='w-11/12'
-                        type="range"
-                        min={0}
-                        max={duration}
-                        value={currentTime}
-                        onChange={handleSeek}
-                    />
-                    <span>{formatTime(duration)}</span>
-                </div>
-
-               <div className='relative flex pl-5'>
-
-                <div>
-                    <p className='text-lg font-bold'>{currentTrack.title}</p>
-                    <p className='text-sm'>{`${currentTrack.artist}`}</p>
-                    <p className='text-sm'>{`${currentTrack.album} - ${currentTrack.releaseYear} `}</p>
-                </div>
-
-
-                <div className="w-1/6 flex absolute top-2 left-1/2 transform -translate-x-1/2 justify-center items-center pt-3">
-                    <audio ref={audioRef}
-                        onTimeUpdate={handleTimeUpdate}
-                        onEnded={() => setIsPlaying(false)}
-                    >
-                        Your browser does not support the audio tag.
-                    </audio>
-                    <div className="flex">
-                        <IoPlaySkipBackSharp fontSize={'30px'} />
-                        {isPlaying ? (
-                            <FaRegPauseCircle onClick={handlePlayPause} fontSize={'30px'} className='hover:text-sky-700' />
-                        ) : (
-                            <FaPlayCircle onClick={handlePlayPause} fontSize={'30px'} className='hover:text-sky-700' />
-                        )}
-
-                        <IoPlaySkipForwardSharp fontSize={'30px'} />
+                    
+                    
+                    <div className="flex justify-center items-center">
+                        <span>{formatTime(currentTime)}</span>
+                        <input className='w-11/12'
+                            type="range"
+                            min={0}
+                            max={duration}
+                            value={currentTime}
+                            onChange={handleSeek}
+                        />
+                        <span>{formatTime(duration)}</span>
                     </div>
 
 
-                </div>
+
+                    <div className='relative flex pl-6 justify-between'>
+
+                        <div>
+                            <p className='text-lg font-bold'>{currentTrack.title}</p>
+                            <p className='text-sm'>{`${currentTrack.artist}`}</p>
+                            <p className='text-sm'>{`${currentTrack.album} - ${currentTrack.releaseYear} `}</p>
+                        </div>
+
+
+                        <div className="w-1/6 flex absolute top-2 left-1/2 transform -translate-x-1/2 justify-center items-center pt-3">
+                            <audio ref={audioRef}
+                                onTimeUpdate={handleTimeUpdate}
+                                onEnded={() => setIsPlaying(false)}
+                            >
+                                Your browser does not support the audio tag.
+                            </audio>
+                            <div className="flex justify-center items-center">
+                                <IoPlaySkipBackSharp fontSize={'20px'} className='mr-2' />
+                                {isPlaying ? (
+                                    <FaRegPauseCircle onClick={handlePlayPause} fontSize={'40px'} className='hover:text-sky-700 mr-2' />
+                                ) : (
+                                    <FaPlayCircle onClick={handlePlayPause} fontSize={'40px'} className='hover:text-sky-700 mr-2' />
+                                )}
+
+                                <IoPlaySkipForwardSharp fontSize={'20px'} className='mr-4' />
+                            </div>
+
+
+
+                        </div>
+
+                        <div className='flex justify-center items-center pr-5'>
+                        <FaVolumeLow
+                            className='mr-4'
+                            fontSize={'20px'}
+                        
+                        />
+
+                        <input
+                        className='border-4'
+                            type="range"
+                            min={0}
+                            max={1}
+                            step={0.01}
+                            value={volume}
+                            onChange={handleVolumeChange}
+                        />
+                        {/* {isVolumeControlVisible && (
+                                   
+                                )} */}
+                    </div>
+
+                    </div>
+                    
+
+                    
 
                 </div>
 
-                </div>
 
-                
 
             </div>
 
