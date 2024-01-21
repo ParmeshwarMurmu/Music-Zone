7e43c6cb7ad42134a9c68bf5139cbaa4b6501b9f
Parmeshwar Murmu <murmuparmeshwar05@gmail.com>
2024-01-21 20:38:49 +0530

    SignUp_Layout_Updated

--- frontend/src/Routes/SignUp.tsx
+++ frontend/src/Routes/SignUp.tsx
@@ -5,6 +5,7 @@ import { AbsoluteCenter, Avatar, Box, Button, Divider, Heading, Input, Wrap, Wra
 import { FcGoogle } from "react-icons/fc";
 import { ImFacebook2 } from "react-icons/im";
 import { FcPhoneAndroid } from "react-icons/fc";
+import { Link } from 'react-router-dom';
 
 export const SignUp = () => {
 
@@ -23,7 +24,7 @@ export const SignUp = () => {
                 </Wrap>
             </div> */}
 
-            <div className='w-full md:w-1/5 lg:w-2/6 m-auto p-2 border-2 '>
+            <div className='w-full md:w-1/5 lg:w-1/4 m-auto p-2 border-2 '>
 
                 <Heading className='mb-4' as='h2' size='xl'>
                     Sign up to start listening
@@ -33,6 +34,11 @@ export const SignUp = () => {
                     <p className='mb-2'>Email Address</p>
                     <Input placeholder='name@gmail.com'
                         type='text'
+                    />
+
+                    <p className='mb-2'>Password</p>
+                    <Input placeholder='Password'
+                        type='password'
                     />
                 </div>
 
@@ -53,7 +59,7 @@ export const SignUp = () => {
                     <Button colorScheme='' size='md' className='w-full mb-4 p-2 outline border-2 hover:border-indigo-300'
                     // isLoading
                     >
-                    <FcPhoneAndroid className='mr-5 text-blue-500' fontSize={'30px'} /> <span className='text-black'>Sign up Using   OTP</span>
+                        <FcPhoneAndroid className='mr-5 text-blue-500' fontSize={'30px'} /> <span className='text-black'>Sign up Using   OTP</span>
                     </Button>
                 </div>
 
@@ -61,19 +67,32 @@ export const SignUp = () => {
                     <Button colorScheme='' size='md' className='w-full mb-4 p-2 outline border-2 hover:border-indigo-300'
                     // isLoading
                     >
-                    <FcGoogle className='mr-5' fontSize={'30px'} /> <span className='text-black'>Sign up with Google</span>
+                        <FcGoogle className='mr-5' fontSize={'30px'} /> <span className='text-black'>Sign up with Google</span>
                     </Button>
                 </div>
 
-                <div>
-                    <Button colorScheme='' size='md' className='w-full mb-4 p-2 outline border-2 hover:border-indigo-300'
+                <div className='mb-6'>
+                    <Button colorScheme='' size='md' className='w-full p-2 outline border-2 hover:border-indigo-300'
                     // isLoading
                     >
-                    <ImFacebook2 className='mr-5 text-blue-500' fontSize={'30px'} /> <span className='text-black'>Sign up with Facebook</span>
+                        <ImFacebook2 className='mr-5 text-blue-500' fontSize={'30px'} /> <span className='text-black'>Sign up with Facebook</span>
                     </Button>
                 </div>
 
-                
+                <hr className='mb-4'></hr>
+
+                <div className='flex justify-center items-center mb-6'>
+                    <p>Already have an account ? <Link to={'/login'}><span className='text-blue-600 underline decoration-solid'>Login here</span></Link></p>
+                </div>
+
+
+
+                <div className='flex mb-6'>
+                    <p className='text-xs text-center'>This site is protected by reCAPTCHA and the Google
+                        Privacy Policy and Terms of Service apply.</p>
+                </div>
+
+
 
 
             </div>
