3cee69e1360938cac891bd6ed02e821b9d629f42
Parmeshwar Murmu <murmuparmeshwar05@gmail.com>
2024-01-24 16:43:06 +0530

    Store

--- frontend/src/Redux/Store/Store.ts
+++ frontend/src/Redux/Store/Store.ts
@@ -1,7 +1,4 @@
-import { applyMiddleware, combineReducers,createStore,  Middleware, Reducer, StoreEnhancer} from "redux";
-import { ThunkMiddleware, thunk } from "redux-thunk";
-// import { reducer as SignUpReducer } from "../SignUpReducer/reducer";
-import { userEmailActionType, userNameActionType, userPasswordActionType } from "../SignUpReducer/action";
+
 import { configureStore } from "@reduxjs/toolkit";
 
 
@@ -16,20 +13,3 @@ export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
 
-
-// // Define the root state type
-// export interface RootState {
-//     SignUpReducer: signUp;
-//     // Add other reducers and their states if needed
-// }
-
-// // Define the root action type
-// export type RootAction =
-//     | userNameActionType
-//     | userPasswordActionType
-//     | userEmailActionType;
-// // Add other action types if needed
-
-// const rootReducer = combineReducers({
-//     SignUpReducer,
-// });
