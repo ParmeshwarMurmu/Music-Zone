125296c5e5c23bbb01214065ed17da62dedc9449
Parmeshwar Murmu <murmuparmeshwar05@gmail.com>
2024-02-12 12:36:16 +0530

    Google_Auth_For_Login

--- frontend/src/Routes/Login.tsx
+++ frontend/src/Routes/Login.tsx
@@ -13,7 +13,7 @@ import { loginEmailAction, loginEmailValueFromReduxStore, loginIsErrorAction, lo
 import { IoMdEyeOff, IoMdEye } from "react-icons/io";
 import axios from 'axios';
 import { APP_URL, USER_LOGIN_ENDPOINT } from '../Endpoints/Endpoints';
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider, signInWithPopup, signInWithRedirect, getRedirectResult } from "firebase/auth";
 import { auth } from '../fireBase/Config'
 
 export const Login = () => {
@@ -103,12 +103,14 @@ export const Login = () => {
 
   // Function to handle login using google Account
 
-  const googleLoginHandler = () => {
-    
+  const googleLoginHandler = async () => {
+
     signInWithPopup(auth, provider)
       .then((result) => {
         // This gives you a Google Access Token. You can use it to access the Google API.
         const credential = GoogleAuthProvider.credentialFromResult(result);
+        console.log(credential);
+        
         // const token = credential.accessToken;
         // The signed-in user info.
         const user = result.user;
@@ -125,6 +127,8 @@ export const Login = () => {
         const credential = GoogleAuthProvider.credentialFromError(error);
         // ...
       });
+
+
 
   }
 
