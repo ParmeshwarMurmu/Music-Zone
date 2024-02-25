552f7e4a4b8e783dc2461411fdf6f26ec8e86c55
Parmeshwar Murmu <murmuparmeshwar05@gmail.com>
2024-02-25 20:29:39 +0530

    Footer

--- frontend/src/Components/Footer/Footer.tsx
+++ frontend/src/Components/Footer/Footer.tsx
@@ -1,5 +1,6 @@
+import { Heading } from '@chakra-ui/react';
 import React from 'react'
-import { FaTwitter, FaFacebook,FaInstagram } from "react-icons/fa";
+import { FaTwitter, FaFacebook, FaInstagram } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa6";
 
 export const Footer = () => {
@@ -8,28 +9,49 @@ export const Footer = () => {
     <div>
 
       <div className='flex justify-center'>
-      <FaTwitter className='m-4 size-6 cursor-pointer hover:scale-125' />
-      <FaFacebook className='m-4 size-6 cursor-pointer hover:scale-125' />
-      <FaInstagram className='m-4 size-6 cursor-pointer hover:scale-125' />
-      <FaLinkedin className='m-4 size-6 cursor-pointer hover:scale-125' />
+        <FaTwitter className='m-4 size-6 cursor-pointer hover:scale-125' />
+        <FaFacebook className='m-4 size-6 cursor-pointer hover:scale-125' />
+        <FaInstagram className='m-4 size-6 cursor-pointer hover:scale-125' />
+        <FaLinkedin className='m-4 size-6 cursor-pointer hover:scale-125' />
       </div>
-        
+
+      <div>
         <div>
-         <div>
-            <h1>Company</h1>
-            <p>About</p>
-            <p>Jobs</p>
-            <p>For the Record</p>
-         </div>
+          <Heading as='h6'>
+            Community
+          </Heading>
+          <p>Blogs</p>
+          <p>Community</p>
+          <p>Developers</p>
         </div>
 
         <div>
-
+          <Heading as='h6'>
+            Company
+          </Heading>
+          <p>About Us</p>
+          <p>Team</p>
+          <p>Media</p>
+          <p>Our Commitments</p>
         </div>
 
         <div>
-
+          <Heading as='h6'>
+            Useful Links
+          </Heading>
+          <p>Terms & Services</p>
+          <p>Policy & Privacy</p>
+         
         </div>
+      </div>
+
+      <div>
+
+      </div>
+
+      <div>
+
+      </div>
 
 
     </div>
